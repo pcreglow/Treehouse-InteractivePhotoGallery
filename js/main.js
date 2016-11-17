35f4f513620e7a0b10c3080397a5de07ff081735
Phillip Creglow <phillipcreglow@msn.com>
2016-11-17 16:18:12 -0500

    Deleted experimental jQuery code for gallery search, added functional jQuery code for gallery search, added p tags to each gallery image for search functionality, refactored with Atom Beautify

--- js/main.js
+++ js/main.js
@@ -1,42 +1,28 @@
-console.log("Hello World!");
+/* Gallery Search */
 
-/* $('.gallery-photo img').each(function() {
-    $(this).attr('caption', $().text().toLowerCase());
+function gallerySearch(gallery) {
+
+    $('input').change(function() {
+            var filter = $(this).val();
+            if (filter) {
+
+                var matches = $(gallery).find('p:Contains(' + filter + ')').parent();
+                $('a', gallery).not(matches).hide();
+                $(matches).show();
+
+            } else {
+                $(gallery).find("a").show();
+            }
+            return false;
+        })
+        .keyup(function() {
+            $(this).change();
+        });
+}
+
+$(function() {
+    gallerySearch($("#gallery"));
 });
-
-$('#photo-search').on('keyup', function() {
-
-    var searchTerm = $(this).val().toLowerCase();
-
-    $('.gallery-photo img').each(function() {
-        if ($(this).filter('[caption *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
-            $(this).show();
-        } else {
-            $(this).hide();
-        }
-    });
-}); */
-
-/* $('#photo-search').on('keyup', function() {
-  var value = this.value;
-  $('.gallery-photo img').hide().each(function() {
-     if ($(this).text().search(value) === ('img[alt="' + value + '"]')) {
-       $(this).show();
-     } else {
-       $(this).hide();
-     }
-  });
-}); */
-
-
-/* $('#photo-search').on('keyup', function() {
-  var value = this.value;
-  if ($('.gallery-photo img').find('[alt="' + value + '"]')) {
-    $(this).show();
-  } else {
-    $(this).hide();
-  }
-}); */
 
 /* Lightbox */
 
