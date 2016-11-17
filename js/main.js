98617e6d411f68f02e5d02e9d1e472dcad60fdec
Phillip Creglow <phillipcreglow@msn.com>
2016-11-16 19:22:40 -0500

    Set opacity for gallery image to .3, add first jQuery search functionality to change opacity based on search term

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@ console.log("Hello World!");
 /* $('h2').on('click', function() {
   $(this).hide();
 }); */
+$('.gallery-photo img').each(function() {
+  $(this).attr('alt', $().text().toLowerCase());
+});
+
+$('#photo-search').on('keyup', function() {
+
+var searchTerm = $(this).val().toLowerCase();
+
+  $('.gallery-photo img').each(function() {
+    if($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+      $(this).css({'opacity': '.3'});
+    } else {
+      $(this).css({'opacity': '1'});
+    }
+  });
+});
