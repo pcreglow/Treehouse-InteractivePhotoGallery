f6540d1d7d70d8b5ebe70ee0aef4ac723f611be1
Phillip Creglow <phillipcreglow@msn.com>
2016-11-16 20:30:46 -0500

    Search functionality with jQuery, partially working but all images hide based on caption instead of assigned images

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 console.log("Hello World!");
-/* $('h2').on('click', function() {
-  $(this).hide();
-}); */
+
 $('.gallery-photo img').each(function() {
-  $(this).attr('alt', $().text().toLowerCase());
+    $(this).attr('caption', $().text().toLowerCase());
 });
 
 $('#photo-search').on('keyup', function() {
 
-var searchTerm = $(this).val().toLowerCase();
+    var searchTerm = $(this).val().toLowerCase();
 
-  $('.gallery-photo img').each(function() {
-    if($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
-      $(this).css({'opacity': '.3'});
-    } else {
-      $(this).css({'opacity': '1'});
-    }
-  });
+    $('.gallery-photo img').each(function() {
+        if ($(this).filter('[caption *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
+    });
 });
