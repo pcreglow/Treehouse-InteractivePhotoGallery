dccaffcbf73c57b62387a32533512c82d2935546
Phillip Creglow <phillipcreglow@msn.com>
2016-11-21 17:21:13 -0500

    Added previous and next arrows to lightbox, refactored main.js file with click event for next arrow and next image function, still have to add previous arrow functionality and limit gallery scroll to images searched

--- js/main.js
+++ js/main.js
@@ -17,11 +17,11 @@ $(document).ready(function() {
                 if (filter) {
 
                     var matches = $(gallery).find('p:contains(' + filter + ')').parent();
-                    $('a', gallery).not(matches).hide();
+                    $('li', gallery).not(matches).hide();
                     $(matches).show();
 
                 } else {
-                    $(gallery).find("a").show();
+                    $(gallery).find("li").show();
                 }
                 return false;
             })
@@ -34,7 +34,7 @@ $(document).ready(function() {
         gallerySearch($("#gallery"));
     });
 
-    /* Lightbox */
+    /* Lightbox and Gallery Controls */
 
     var $lightbox = $('<div id="lightbox"></div>');
     var $previous = $('<p class="previous"><</p>')
@@ -42,19 +42,55 @@ $(document).ready(function() {
     var $caption = $('<p></p>');
     var $next = $('<p class="next">></p>')
 
-    $lightbox.append($previous);
+
     $lightbox.append($next);
+    $lightbox.append($previous);
     $lightbox.append($image);
     $lightbox.append($caption);
     $("body").append($lightbox);
 
-    $(".gallery-photo a").click(function(event) {
+    $(".gallery-photo").click(function(event) {
         event.preventDefault();
-        var photoLocation = $(this).attr("href");
+        var photoLocation = $(this).children("a").attr("href");
         $image.attr("src", photoLocation);
         $lightbox.show();
+        getLightboxImage(this);
         var captionText = $(this).children("p").text();
         $caption.text(captionText);
+
+
+    $next.click(function(event) {
+      event.stopPropagation();
+      slideNextImage();
+      $image.show();
+      $caption.text(getLightboxCaption);
+    });
+
+    $previous.click(function(event) {
+      event.stopPropagation();
+      slidePreviousImage();
+    });
+
+    function getLightboxImage(image) {
+      lightboxImage = $(image).children("a").next();
+      lightboxCaption = $(image).children("p").next().text();
+      lightboxImageLocation = $(lightboxImage).attr("href");
+      $image.attr("src", lightboxImageLocation);
+      $caption.text(lightboxCaption);
+    }
+
+    function slideNextImage() {
+      getNextImageParent = $(lightboxImage).parent().next();
+      getLightboxCaption = $(getNextImageParent).children("p").text();
+      getNextImageLocation = $(getNextImageParent).children("a").attr("href");
+      $image.attr("src", getNextImageLocation);
+      getLightboxImage(getNextImageParent);
+    }
+
+    function slidePreviousImage() {
+
+    }
+
     });
 
     $lightbox.click(function() {
